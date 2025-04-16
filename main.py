46d8f668542ab4f8153d73432265f21b74cbc25c
Oleksandr Tsakhlo <richardkvitka55@gmail.com>
2025-04-16 04:15:57 +0200

    Add files via upload
    + Save best score 
    + Game restarts automatically

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
+screen.onkey(snake.left, "Left")
 screen.onkey(snake.down, "Down")
-screen.onkey(snake.left,"Left")
 screen.onkey(snake.right, "Right")
 
 
@@ -31,17 +31,16 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        scoreboard.update()
+        scoreboard.increase_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -290 or snake.head.ycor() > 285 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     for segment in snake.segments[1::]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
-
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
 
--- scoreboard.py
+++ scoreboard.py
@@ -5,25 +5,41 @@ FONT = ("Courier", 24, "normal")
 POSITION = (0, 275)
 
 
+def set_highscore():
+    file = open("data.txt")
+    content = file.read()
+    return content
+
+
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = set_highscore()
         self.color("white")
         self.penup()
         self.hideturtle()
         self.setposition(POSITION)
-        self.write(f"Score: {self.score} ",  align=ALIGNMENT,font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.highscore}",  align=ALIGNMENT,font=FONT)
 
     def update(self):
         self.clear()
-        self.score += 1
-        self.write(f"Score: {self.score} ", align=ALIGNMENT,font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.highscore}",  align=ALIGNMENT,font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > int(self.highscore):
+            self.highscore = self.score
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.highscore}")
+
+        self.score = 0
+        self.update()
+
+    def increase_score(self):
+        self.score += 1
+        self.update()
+
 
 
 
